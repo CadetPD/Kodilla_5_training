1ab3332f69912d90d8812733f5a1a67cc4230e36
Przemek Dorski <przemek.dorski@o2.pl>
2021-11-11 23:55:31 +0100

    Classes first step

---
+++ cwiczenie_5-1_1.py
@@ -0,0 +1,12 @@
+fn = ["Czesław", "Ludwik", "Piotr", "Augustyna", "Julitta"]
+ln = ["Jasinski", "Borkowski", "Zajac", "Zawadzka", "Adamczyk"]
+e_m = ["CzeslawJasinski@jourrapide.com", "LudwikBorkowski@dayrep.com", "PiotrZajac@teleworm.us", "AugustynaZawadzka@dayrep.com", "JulittaAdamczyk@dayrep.com"]
+
+class Vcards:
+    def __init__(self, first_name, last_name, e_mail):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.e_mail = e_mail
+        print(f"Imię i nazwisko: {self.first_name} {self.last_name}, email: {self.e_mail}")
+
+cards = [Vcards(first_name, last_name, e_mail) for first_name, last_name, e_mail in zip(fn, ln, e_m)]
