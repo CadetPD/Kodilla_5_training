6126b53777a6a20225244f79f87e33330e2ccd02
cadetpd <przemek.dorski@o2.pl>
2021-11-12 10:26:48 +0100

    Small change in __init__

--- cwiczenie_5-1_1.py
+++ cwiczenie_5-1_1.py
@@ -4,9 +4,8 @@ e_m = ["CzeslawJasinski@jourrapide.com", "LudwikBorkowski@dayrep.com", "PiotrZaj
 
 class Vcards:
     def __init__(self, first_name, last_name, e_mail):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.e_mail = e_mail
-        print(f"Imię i nazwisko: {self.first_name} {self.last_name}, email: {self.e_mail}")
+        self.first_name, self.last_name, self.e_mail = first_name, last_name, e_mail
+        
+        print(f"Imię i nazwisko: {first_name} {last_name}, email: {e_mail}")
 
 clients = [Vcards(first_name, last_name, e_mail) for first_name, last_name, e_mail in zip(fn, ln, e_m)]
